58496abedba6ecc453d30a62f9fd76fc74d65ec0
caihuag <39232479+caihuag@users.noreply.github.com>
2019-01-22 15:34:25 +0800

    Add files via upload

--- WIDCAT_Split.py
+++ WIDCAT_Split.py
@@ -27,40 +27,37 @@ kmax = int(k * MAP_SIZE * MAP_SIZE)
 kmin = int(k * MAP_SIZE * MAP_SIZE)
 alpha = 0.4
 EPOCH_NUM = 20
+def class_wise(x):
+    multi_map = tf.split(x, num_or_size_splits=C, axis=3)  # list of [batch_size, h, w, M]
+    class_wise_pool_out = tf.reduce_mean(multi_map[0], axis=3)  # class_wise_pool, [batch_size, h, w]
+    t = tf.reshape(class_wise_pool_out, (-1, MAP_SIZE * MAP_SIZE))
+    return t
+def spatial_pooling(x):
+    # outlist=[]
+    sum1 = tf.nn.top_k(x, kmax).values  # [batch_size, kmax]
+    sum2 = -tf.nn.top_k(-x, kmin).values  # [batch_size, kmin]
+    score = tf.reduce_mean(sum1, axis=1) + alpha * tf.reduce_mean(sum2, axis=1)  # [batch_size]
+    scores = tf.reshape(score, (-1, 1))
+    # scores=tf.expand_dims(score,1)
+    # outlist.append(score)
+    # outlist=np.array(outlist)
+    return scores
+def lr_scheduler(epoch):
+    if epoch < 1:
+        lr = 2e-4
+    else:
+        lr = 2e-5
+    return lr
+
 if __name__=="__main__":
     datasets = dataset("/home/gwj/NEC_DATA/", validation_size=0.3)
     A = datasets.load_data()
     validation_exam_no, validation_imgs, validation_labels = A[:3]
     train_exam_no, train_images, train_labels = A[3:]
-
-
-    def class_wise(x):
-        multi_map = tf.split(x, num_or_size_splits=C, axis=3)  # list of [batch_size, h, w, M]
-        class_wise_pool_out = tf.reduce_mean(multi_map[0], axis=3)  # class_wise_pool, [batch_size, h, w]
-        t = tf.reshape(class_wise_pool_out, (-1, MAP_SIZE * MAP_SIZE))
-        return t
-
-
-    def spatial_pooling(x):
-        # outlist=[]
-        sum1 = tf.nn.top_k(x, kmax).values  # [batch_size, kmax]
-        sum2 = -tf.nn.top_k(-x, kmin).values  # [batch_size, kmin]
-        score = tf.reduce_mean(sum1, axis=1) + alpha * tf.reduce_mean(sum2, axis=1)  # [batch_size]
-        scores = tf.reshape(score, (-1, 1))
-        # scores=tf.expand_dims(score,1)
-        # outlist.append(score)
-        # outlist=np.array(outlist)
-        return scores
-
-
     base_model = ResNet50(include_top=False, weights='imagenet', input_shape=(224, 224, 3))
     x = base_model.output  # FCN, [batch_size, h, w, 2048]
     x = Conv2D(M * C, (1, 1))(x)  # WSL transfer, [batch_size, h, w, 40]
-    import tensorflow as tf
-
     x = Lambda(class_wise)(x)
-    import tensorflow as tf
-
     x = Lambda(spatial_pooling)(x)
     x = Activation('sigmoid')(x)
     model = Model(inputs=base_model.input, outputs=x)
@@ -68,16 +65,6 @@ if __name__=="__main__":
     model.compile(loss='binary_crossentropy',
                   optimizer=opt,
                   metrics=['accuracy'])
-
-
-    def lr_scheduler(epoch):
-        if epoch < 1:
-            lr = 2e-4
-        else:
-            lr = 2e-5
-        return lr
-
-
     reduce_lr = LearningRateScheduler(lr_scheduler)
     early_stop = EarlyStopping(monitor='val_loss', patience=2, verbose=1)
 
