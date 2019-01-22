93034a0c88c7fd380b5b456c02491027b053da9f
caihuag <39232479+caihuag@users.noreply.github.com>
2019-01-22 15:41:33 +0800

    Add files via upload

--- Predict_Nec.py
+++ Predict_Nec.py
@@ -3,8 +3,9 @@ from keras.utils import plot_model
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
-
 import tensorflow  as tf
 from keras.models import  load_model
-model1=load_model('/home/gwj/NEC_model/WILDCAT.h5',custom_objects={'class_wise':class_wise,'spatial_pooling':spatial_pooling})
-print(model1.summary())
+from WIDCAT_Split import class_wise,spatial_pooling 
+if __name__=="__main__":
+     model1=load_model('/home/gwj/NEC_model/WILDCAT.h5',custom_objects={'class_wise':class_wise,'spatial_pooling':spatial_pooling})
+     print(model1.summary())
