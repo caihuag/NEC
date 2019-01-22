8be999000c3ff988c6da5aad7a6ee36197a91aff
caihuag <39232479+caihuag@users.noreply.github.com>
2019-01-22 12:38:52 +0800

    Add files via upload

---
+++ Predict_Nec.py
@@ -0,0 +1,10 @@
+from keras.applications.vgg16 import VGG16
+from keras.utils import plot_model
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense
+
+import tensorflow  as tf
+from keras.models import  load_model
+model1=load_model('/home/gwj/NEC_model/WILDCAT.h5',custom_objects={'class_wise':class_wise,'spatial_pooling':spatial_pooling})
+print(model1.summary())
